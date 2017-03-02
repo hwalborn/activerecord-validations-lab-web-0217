58633bc0ee373503eeb950cd77810d29dbe07162
hwalborn <hwalborn@gmail.com>
2017-03-02 08:52:28 -0500

    what happened

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait
+
+  def clickbait
+    arr = ["Won't Believe", "Secret", "Top", "Guess"]
+    arr.each do |word|
+      if title && !title.include?(word)
+        errors.add(:title, "Stop it!")
+        return false
+      else
+        return true
+      end
+    end
+  end
 end
